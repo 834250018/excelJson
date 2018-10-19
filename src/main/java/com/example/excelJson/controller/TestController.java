9a8f7ee9d58f0efba39e966d3538fdf8898a5ae2
weiye <834250018@qq.com>
2018-10-19 21:58:29 +0800

    1..

--- src/main/java/com/example/excelJson/controller/TestController.java
+++ src/main/java/com/example/excelJson/controller/TestController.java
@@ -16,6 +16,7 @@ public class TestController {
     public Boolean doSome(@RequestBody Object body) {
         System.out.println("调用了什么什么,打印出什么" + new Date());
         System.out.println("测试上传会调用");
+        System.out.println("测试上传会调用");
         return true;
     }
 }
